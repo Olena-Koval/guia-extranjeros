e5fd238d791ee59ee71d3e4dbf58ee3c99563a01
Olena-Koval <19111982e@gmail.com>
2025-03-12 14:55:04 +0100

    Los cambios de fichero Ayudas

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 // src/axios.js
-import axios from 'axios';
+import axios from "axios";
 
 // Configuramos Axios con una base URL para las solicitudes
 const instance = axios.create({
-  baseURL: 'https://olena-koval.github.io/guia-extranjeros',  // Sustituye esta URL por la URL de tu API real
+  baseURL: "https://olena-koval.github.io/guia-extranjeros", // Sustituye esta URL por la URL de tu API real
 });
 
 export default instance;
