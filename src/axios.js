7eccbd3e39d0b2c93813172967a4418c687943be
Olena-Koval <19111982e@gmail.com>
2025-03-12 13:04:30 +0100

    Updates

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-// src/axios.js
-import axios from 'axios';
-
-// Configuramos Axios con una base URL para las solicitudes
-const instance = axios.create({
-  baseURL: 'https://api.example.com',  // Sustituye esta URL por la URL de tu API real
-});
-
-export default instance;
